237f2101f2eb32e9ff731703b3f577216b276144
schatteburg <stom@student.ethz.ch>
2023-10-05 21:00:02 +0200

    removed afield argument, integrating dimensions now running, still checking

--- overlap_integrals.py
+++ overlap_integrals.py
@@ -20,21 +20,25 @@ class field():
             self.shape = fieldvalues.shape
         
         # check number of dimensions and coordinate system are supported
-        if self.ndims == 1 and coord_sys not in ["cartesian", "polar"]  or self.ndims == 2 and coord_sys not in ["cartesian", "polar"] or self.ndims >= 3 or self.ndims <=0:
+        if self.ndims == 1 and coord_sys not in ["cartesian", "polar"]  or self.ndims == 2 and coord_sys not in ["cartesian", "polar"] or self.ndims == 3 and coord_sys not in ["cartesian", "cylindrical"] or self.ndims > 3 or self.ndims <=0:
             raise ValueError(f'Number of dimensions ({self.ndims}) and/or coordinate system ({coord_sys}) are not supported.')
 
         self.values = fieldvalues
         self.var = var
         self.cs = coord_sys
     
-    def integrate_area(self, afield = None):
-        if afield is None:
-            return self.integrate_area(afield=self)
-        
-        if afield.ndims == 1:
-            if afield.cs == "cartesian":
-                return np.trapz(afield.values, x=afield.var[0])
-            elif afield.cs == "polar":
-                raise NotImplementedError(f"Coordinate system {afield.cs} is not implemented.")
+    def integrate_all_dimensions(self):
+        if self.ndims == 1:
+            if self.cs == "cartesian":
+                print('integrating dimension 0')
+                return np.trapz(self.values, x=self.var[0])
+            elif self.cs == "polar":
+                raise NotImplementedError(f"Coordinate system {self.cs} is not implemented.")
         else:
-            return self.integrate_area(field(afield.values, afield.var[::-2], afield.cs)) # TODO: does not work to create new field with mismatched dimensions
+            print(f'integrating dimension {self.ndims-1}')
+            newvalues = np.trapz(self.values, x=self.var[-1], axis=self.ndims-1)
+            # print(newvalues.shape)
+            # print(self.ndims-1)
+            # print(self.var)
+            # print(self.var[:-1])
+            return field(newvalues, self.var[:-1], self.cs).integrate_all_dimensions() # recursive call
